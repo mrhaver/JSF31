77be0b4dc9e09932f7d3144f7701d655a4e4e435
mrhaver <f.haver@student.fontys.nl>
2015-09-23 18:08:02 +0200

    t/m 6

--- JSF31KochFractalFX - startup/src/calculate/KochManager.java
+++ JSF31KochFractalFX - startup/src/calculate/KochManager.java
@@ -5,9 +5,11 @@
  */
 package calculate;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 import jsf31kochfractalfx.JSF31KochFractalFX;
+import timeutil.TimeStamp;
 
 /**
  *
@@ -17,28 +19,41 @@ public class KochManager implements Observer{
     
     private JSF31KochFractalFX application;
     private KochFractal koch;
+    private ArrayList<Edge> edges;
+    private TimeStamp ts;
     
     public KochManager(JSF31KochFractalFX application) {
         this.application = application;
         koch = new KochFractal();
         koch.addObserver(this);
+        edges = new ArrayList<Edge>();
     }
     
     public void changeLevel(int nxt) {
         koch.setLevel(nxt);
+        ts = new TimeStamp();
+        ts.setBegin("Begin Meting");
         drawEdges();
+        ts.setEnd("Fractal Getekend");
+        application.setTextCalc(ts.toString());
+        application.setTextNrEdges(String.valueOf(koch.getNrOfEdges()));
     }
     
     public void drawEdges() {
         application.clearKochPanel();
+        edges.clear();
         koch.generateLeftEdge();
         koch.generateBottomEdge();
         koch.generateRightEdge();
+        for(Edge e : edges){
+            application.drawEdge(e);
+        }
     }
 
     @Override
     public void update(Observable o, Object arg) {
-        application.drawEdge((Edge)arg);
+        //application.drawEdge((Edge)arg);
+        edges.add((Edge)arg);
     }
 
 
