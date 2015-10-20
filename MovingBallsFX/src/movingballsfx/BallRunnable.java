e0701a3352964b765c051f4fab8a44f0a2a75825
alexras95 <alexras@live.nl>
2015-10-20 09:39:05 +0200

    MovingBallzzz

---
+++ MovingBallsFX/src/movingballsfx/BallRunnable.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movingballsfx;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Peter Boots
+ */
+public class BallRunnable implements Runnable {
+
+    private Ball ball;
+    private Monitor monitor;
+
+    public BallRunnable(Ball ball, Monitor monitor) {
+        this.ball = ball;
+        this.monitor = monitor;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                if(ball.getColor() == Color.RED && ball.isEnteringCs()){
+                    monitor.enterReader();
+                }
+                else if(ball.getColor() == Color.RED && ball.isLeavingCs()){
+                    monitor.exitReader();
+                }
+                else if(ball.getColor() == Color.BLUE && ball.isEnteringCs()){
+                    monitor.enterWriter();
+                }
+                else if(ball.getColor() == Color.BLUE && ball.isLeavingCs()){
+                    monitor.exitWriter();
+                }
+                ball.move();
+                
+                Thread.sleep(ball.getSpeed());
+                
+            } catch (InterruptedException ex) {
+                if(ball.getXPos() <= ball.getMaxCsX() && ball.getXPos() >= ball.getMinCsX()){
+                    if(ball.getColor() == Color.BLUE){
+                        try {
+                            monitor.exitWriter();
+                        } catch (InterruptedException ex1) {
+                            System.out.println(ex1.toString());
+                        }
+                    }
+                    if(ball.getColor() == Color.RED){
+                        try {
+                            monitor.exitReader();
+                        } catch (InterruptedException ex1) {
+                            System.out.println(ex1.toString());
+                        }
+                    }
+                }
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
---
+++ MovingBallsFX/src/movingballsfx/Monitor.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movingballsfx;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author Alex Ras
+ */
+public class Monitor {
+    
+    private int readersActive;
+    private int writersActive;
+    Lock monLock = new ReentrantLock();
+    Condition okToRead = monLock.newCondition();
+    Condition okToWrite = monLock.newCondition();
+    private int writersWaiting;
+    
+    public Monitor(){
+        
+    }
+    
+    public void enterReader() throws InterruptedException{
+        
+        monLock.lock();
+        try{
+            while(writersActive > 0){
+                okToRead.await();
+                
+            }
+            readersActive++;
+        }
+        finally{
+            monLock.unlock();
+        }
+    }
+    
+    public void exitReader() throws InterruptedException{
+        monLock.lock();
+        try{
+            readersActive--;
+            if(readersActive == 0){
+                okToWrite.signal();
+            }
+        }
+        finally{
+            monLock.unlock();
+        }
+    }
+    
+    public void enterWriter() throws InterruptedException{
+        
+        monLock.lock();
+        try{
+            while(writersActive > 0 || readersActive > 0){
+                writersWaiting++;
+                okToWrite.await();
+                writersWaiting--;
+            }
+            writersActive++;
+        }
+        finally{
+            monLock.unlock();
+        }
+    }
+    
+    public void exitWriter() throws InterruptedException{
+        monLock.lock();
+        
+        try{
+            writersActive--;
+            if(writersWaiting > 0){ 
+                okToWrite.signal();
+            }
+            else{
+                okToRead.signalAll();
+            }
+        }
+        finally{
+            monLock.unlock();
+        }
+    }
+}
